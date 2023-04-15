4d07199a260e182d3a54e9ae9b7d387191d17532
Gabriel Chaves <gabrielbum@gmail.com>
2023-04-15 18:41:46 -0300

    Removed warnings

--- test/todo/view/todo_page_test.dart
+++ test/todo/view/todo_page_test.dart
@@ -1,9 +1,5 @@
-import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:premium_todo/todo/forms/todo_form.dart';
 import 'package:premium_todo/todo/model/todo_model.dart';
 
 import 'package:premium_todo/todo/todo.dart';
@@ -15,7 +11,8 @@ void main() {
   group('TodoView', () {
     testWidgets('should render todos list', (tester) async {
       final getTodosUC = mockGetTodosUC(
-          [TodoModel(name: 'Todo 1'), TodoModel(name: 'Todo 2')]);
+        [TodoModel(name: 'Todo 1'), TodoModel(name: 'Todo 2')],
+      );
       final todoBloc = mockTodoBloc(getTodosUC: getTodosUC);
 
       await tester.pumpApp(
